6a9e14ce05d124604c621a7f4900eb1e2ac9ebf7
kszaq <kszaquitto@gmail.com>
2015-06-09 14:14:42 +0200

    [aml/wifi] Add a dummy WiFi driver
    Loading the module toggles WiFi power and triggers SDIO bus rescan.
    Useful when including both Broadcom and Realtek drivers in kernel.

---
+++ drivers/amlogic/wifi/wifi_dummy.c
@@ -0,0 +1,32 @@
+#include <linux/delay.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("kszaq");
+MODULE_DESCRIPTION("Amlogic WiFi power on and SDIO rescan module");
+
+extern void wifi_setup_dt(void);
+extern void extern_wifi_set_enable(int);
+extern void sdio_reinit(void);
+
+static int __init wifi_dummy_init(void)
+{
+	printk(KERN_INFO "Triggered SDIO WiFi power on and bus rescan.\n");
+	wifi_setup_dt();
+	msleep(300);
+	extern_wifi_set_enable(0);
+	msleep(300);
+	extern_wifi_set_enable(1);
+	sdio_reinit();
+	return 0;
+}
+
+static void __exit wifi_dummy_cleanup(void)
+{
+    printk(KERN_INFO "Cleaning up module.\n");
+}
+
+module_init(wifi_dummy_init);
+module_exit(wifi_dummy_cleanup);
