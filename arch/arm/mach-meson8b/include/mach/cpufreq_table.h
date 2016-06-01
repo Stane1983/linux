f083d8e293e1503abd8bb7f7a1c09848651c41ca
kszaq <kszaquitto@gmail.com>
2016-06-01 12:03:27 +0200

    meson8b: change available frequencies for fix_syspll after 16a92a1ea73dc03be51ac8d7610a05aa82e8af72

--- arch/arm/mach-meson8b/include/mach/cpufreq_table.h
+++ arch/arm/mach-meson8b/include/mach/cpufreq_table.h
@@ -18,17 +18,14 @@ static struct cpufreq_frequency_table meson_freq_table[]=
 #ifdef CONFIG_FIX_SYSPLL
 static struct cpufreq_frequency_table meson_freq_table_fix_syspll[]=
 {
-    {0	,   96000   },
-    {1	,  192000   },
-    {2  ,  384000   },
-    {3  ,  768000   },
-#if 1
-    {4  , 1250000   },
-    {5  , 1536000   },
-    {6	, CPUFREQ_TABLE_END},
-#else
-    {4  , 1536000   },
-    {5	, CPUFREQ_TABLE_END},
-#endif
+    {0	, 600000    },
+    {1	, 720000   },
+    {2	, 816000   },
+    {3	, 1008000   },
+    {4	, 1200000   },
+    {5	, 1320000   },
+    {6	, 1488000   },
+    {7	, 1536000   },
+    {8  , CPUFREQ_TABLE_END},
 };
 #endif
